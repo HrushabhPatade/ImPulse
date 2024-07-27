61a5754606d49819216947ed9ecda6cea10fe43f
HrushabhPatade <phrushabh13@gmail.com>
2024-07-27 21:57:57 +0530

    Database setup and testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDb from "./config/dbConfig";
+import e from "cors";
 
 dotenv.config();
+connectDb();
 
 const port = process.env.PORT || 5001;
 
 const app = express();
+app.use(express.json);
+app.use(express.urlencoded);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-app.get("/", (req, res) => {
-  res.send("Hello Blockenthu");
-});
+app.use("/api/");
 
 app.listen(port, () => {
   console.log(`Server started at ${port}`);
